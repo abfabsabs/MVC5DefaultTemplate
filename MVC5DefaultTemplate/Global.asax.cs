db6258a82cea5b19620c7fda281a64f6c255228b
Sabrina Veksler <SVeksler@stanfordmed.org>
2014-05-13 14:12:59 -0700

    with Global.asax recommendations added

--- MVC5DefaultTemplate/Global.asax.cs
+++ MVC5DefaultTemplate/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
 
 namespace MVC5DefaultTemplate
 {
@@ -13,6 +14,8 @@ namespace MVC5DefaultTemplate
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
